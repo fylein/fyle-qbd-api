d8c84bf3e097d4c724bfd2e7323946c7cb3848ba
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2022-12-05 16:12:40 +0530

    Adding support for Journal Entries

--- tests/test_utils/test_iif_generator.py
+++ tests/test_utils/test_iif_generator.py
@@ -20,7 +20,7 @@ def test_generate_iif_file_bills():
     """
     Test the generate_iif_file function
     """
-    file_path = '/tmp/test_generator.csv'
+    file_path = '/tmp/test_generator_bills.iif'
     transactions = fixtures['bills']
 
     iif_generator = QBDIIFGenerator(file_path)
@@ -41,12 +41,32 @@ def test_generate_iif_file_credit_card_purchases():
     """
     Test the generate_iif_file function
     """
-    file_path = '/tmp/test_generator.csv'
+    file_path = '/tmp/test_generator_ccp.iif'
     transactions = fixtures['credit_card_purchases']
 
     iif_generator = QBDIIFGenerator(file_path)
 
     generated_lines = iif_generator.generate_iif_file(transactions, 'CREDIT_CARD_PURCHASE')
+
+    with open(file_path, 'r') as iif_file:
+        lines = list(reader(iif_file, delimiter='\t'))
+
+        assert len(lines) == len(generated_lines)
+
+        for index in range(0, len(lines)):
+            generated_lines[index] = [str(element) for element in generated_lines[index]]
+            assert lines[index] == generated_lines[index]
+
+def test_generate_iif_file_journal_entries():
+    """
+    Test the generate_iif_file function
+    """
+    file_path = '/tmp/test_generator_journal.iif'
+    transactions = fixtures['journal_entries']
+
+    iif_generator = QBDIIFGenerator(file_path)
+
+    generated_lines = iif_generator.generate_iif_file(transactions, 'JOURNAL_ENTRY')
 
     with open(file_path, 'r') as iif_file:
         lines = list(reader(iif_file, delimiter='\t'))
