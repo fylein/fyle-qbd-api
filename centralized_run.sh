7c3aece9d272ba6a35de53ef989aa3078509c485
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2023-02-17 13:11:56 +0530

    Serve application using gunicorn (#35)

---
+++ centralized_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run db migrations
+python manage.py migrate
+
+# Creating the cache table
+python manage.py createcachetable --database cache_db
+
+# Running development server
+gunicorn -c gunicorn_config.py quickbooks_desktop_api.wsgi -b 0.0.0.0:8008
