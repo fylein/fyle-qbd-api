fbde61ddef0f3c3811bfd8327ad102bf2c995431
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2023-05-12 13:47:36 +0530

    Order the iif files by date QBD (#49)
    * change to get to purpose function to take merchant field
    
    * remove debug statements
    
    * improving the code for error
    
    * using the lastest version of inegrations platform connector
    
    * added approved at param for the lastest changes
    
    * added a boolean field in export settings model and created migration
    
    * lastest version of flye-accounting-mappings
    
    * right code for the approved state
    
    * removed space
    
    * ordered the iif files by date
    
    * use updated instead of created_at
    
    ---------
    
    Co-authored-by: Fyle <fyle@Fyles-MacBook-Air.local>
    Co-authored-by: Ashutosh619-sudo <ashutoshsinghkrkt@gmail.com>

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -50,7 +50,7 @@ class AccountingExportView(generics.ListAPIView):
 
         return AccountingExport.objects.filter(
             **filters
-        ).all()
+        ).all().order_by("-updated_at")
 
 
 class AccountingExportDownloadView(generics.CreateAPIView):
