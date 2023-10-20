e3014c536c9be802bb9e8be1e7f1813fe184579f
ruuushhh <rushikeshtayade2001@gmail.com>
2023-10-20 11:37:22 +0530

    Gevent changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -107,15 +107,6 @@ def default_session_fixture(request):
     )
     patched_5.__enter__()
 
-    def unpatch():
-        patched_1.__exit__()
-        patched_2.__exit__()
-        patched_3.__exit__()
-        patched_4.__exit__()
-        patched_5.__exit__()
-
-    request.addfinalizer(unpatch)
-
 
 @pytest.fixture
 @pytest.mark.django_db(databases=['default'])
