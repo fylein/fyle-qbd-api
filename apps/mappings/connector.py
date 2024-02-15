987dbbe4af8bf6c7b56ab4dfc7afb3c3eb2530ae
Hrishabh Tiwari <hrishabh.t@fyle.in>
2024-02-15 10:37:41 +0530

    removed retry exception from connector

--- apps/mappings/connector.py
+++ apps/mappings/connector.py
@@ -1,15 +1,9 @@
-import logging
-
 from django.conf import settings
 from fyle.platform import Platform
-from fyle.platform.exceptions import RetryException
 
 from apps.workspaces.models import FyleCredential
 
 from .models import QBDMapping
-
-logger = logging.getLogger(__name__)
-logger.level = logging.INFO
 
 
 class PlatformConnector:
@@ -33,29 +27,25 @@ class PlatformConnector:
         Sync Cards will sync the corporate cards details from fyle to qbd db
         """
 
-        try:
-            query = {
-                'order': 'updated_at.desc',
-            }
-            generator = self.platform.v1beta.admin.corporate_cards.list_all(query)
-            for items in generator:
-                card_attributes = []
-                unique_card_numbers = []
-                for card in items['data']:
-                    value = '{} - {}'.format(
-                        card['bank_name'],
-                        card['card_number'][-6:].replace('-', '')
-                    )
+        query = {
+            'order': 'updated_at.desc',
+        }
+        generator = self.platform.v1beta.admin.corporate_cards.list_all(query)
+        for items in generator:
+            card_attributes = []
+            unique_card_numbers = []
+            for card in items['data']:
+                value = '{} - {}'.format(
+                    card['bank_name'],
+                    card['card_number'][-6:].replace('-', '')
+                )
 
-                    if value not in unique_card_numbers:
-                        unique_card_numbers.append(value)
-                        card_attributes.append({
-                            'attribute_type': 'CORPORATE_CARD',
-                            'value': value,
-                            'source_id': card['id'],                 
-                        })
-                if len(card_attributes) > 0:
-                    QBDMapping.update_or_create_mapping_objects(card_attributes, self.workspace_id)
-
-        except RetryException:
-            logger.info('RetryException in sync_corporate_card')
+                if value not in unique_card_numbers:
+                    unique_card_numbers.append(value)
+                    card_attributes.append({
+                        'attribute_type': 'CORPORATE_CARD',
+                        'value': value,
+                        'source_id': card['id'],                 
+                    })
+            if len(card_attributes) > 0:
+                QBDMapping.update_or_create_mapping_objects(card_attributes, self.workspace_id)
