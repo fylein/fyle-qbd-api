79cb93b089f7a9bfe9f317d3e9ee0fcb986584ae
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2024-06-05 12:42:24 +0530

    Remove settlement from QBD (#79)
    * Remove settlement from QBD
    
    * Change integrations-platform-connector version
    
    * change version
    
    * change accounting mapping version

--- apps/fyle/models.py
+++ apps/fyle/models.py
@@ -40,7 +40,6 @@ class Expense(models.Model):
     currency = models.CharField(max_length=5, help_text='Home Currency')
     foreign_amount = models.FloatField(null=True, help_text='Foreign Amount')
     foreign_currency = models.CharField(null=True, max_length=5, help_text='Foreign Currency')
-    settlement_id = models.CharField(max_length=255, help_text='Settlement ID', null=True)
     reimbursable = models.BooleanField(default=False, help_text='Expense reimbursable or not')
     state = models.CharField(max_length=255, help_text='Expense state')
     vendor = models.CharField(max_length=255, null=True, blank=True, help_text='Vendor')
@@ -93,7 +92,6 @@ class Expense(models.Model):
                     'currency': expense['currency'],
                     'foreign_amount': expense['foreign_amount'],
                     'foreign_currency': expense['foreign_currency'],
-                    'settlement_id': expense['settlement_id'],
                     'reimbursable': expense['reimbursable'],
                     'state': expense['state'],
                     'vendor': expense['vendor'][:250] if expense['vendor'] else None,
