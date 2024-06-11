6103a9233e39389186bb50de5a7142cab2c909ac
Hrishabh Tiwari <hrishabh.t@fyle.in>
2024-06-11 10:08:58 +0530

    Fix tax amount none rounding

--- apps/fyle/models.py
+++ apps/fyle/models.py
@@ -108,7 +108,7 @@ class Expense(models.Model):
                     'fund_source': SOURCE_ACCOUNT_MAP[expense['source_account_type']],
                     'verified_at': expense['verified_at'],
                     'custom_properties': expense['custom_properties'],
-                    'tax_amount': round(expense['tax_amount'], 2),
+                    'tax_amount': round(expense['tax_amount'], 2) if expense['tax_amount'] is not None else None,
                     'tax_group_id': expense['tax_group_id'],
                     'billable': expense['billable'] if expense['billable'] else False
                 }
