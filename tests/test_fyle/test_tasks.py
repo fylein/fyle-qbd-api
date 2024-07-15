c0140e6dc74480b153b76835432776391630ee03
Hrishabh Tiwari <hrishabh.t@fyle.in>
2024-07-15 10:09:20 +0530

    uncomment test case for coverage

--- tests/test_fyle/test_tasks.py
+++ tests/test_fyle/test_tasks.py
@@ -255,50 +255,51 @@ def test_support_post_date_integration(
     assert journals[0].date.strftime("%m/%d/%Y") == '05/06/2022'
 
 
-# def test_update_non_exported_expenses(db, create_temp_workspace, mocker, api_client):
-#     expense = fixtures['raw_expense']
-#     default_raw_expense = fixtures['default_raw_expense']
-#     org_id = expense['org_id']
-#     payload = {
-#         "resource": "EXPENSE",
-#         "action": 'UPDATED_AFTER_APPROVAL',
-#         "data": expense,
-#         "reason": 'expense update testing',
-#     }
+def test_update_non_exported_expenses(db, create_temp_workspace, mocker, api_client):
+    expense = fixtures['raw_expense']
+    default_raw_expense = fixtures['default_raw_expense']
+    org_id = expense['org_id']
+    payload = {
+        "resource": "EXPENSE",
+        "action": 'UPDATED_AFTER_APPROVAL',
+        "data": expense,
+        "reason": 'expense update testing',
+    }
 
-#     expense_created, _ = Expense.objects.update_or_create(
-#         org_id=org_id,
-#         expense_id='txhJLOSKs1iN',
-#         workspace_id=1,
-#         defaults=default_raw_expense
-#     )
-#     expense_created.exported = False
-#     expense_created.save()
+    expense_created, _ = Expense.objects.update_or_create(
+        org_id=org_id,
+        expense_id='txhJLOSKs1iN',
+        workspace_id=1,
+        defaults=default_raw_expense
+    )
+    expense_created.exported = False
+    expense_created.save()
 
-#     workspace = Workspace.objects.filter(id=1).first()
-#     workspace.org_id = org_id
-#     workspace.save()
+    workspace = Workspace.objects.filter(id=1).first()
+    workspace.org_id = org_id
+    workspace.save()
 
-#     assert expense_created.category == 'Old Category'
+    assert expense_created.category == 'Old Category'
 
-#     update_non_exported_expenses(payload['data'])
+    update_non_exported_expenses(payload['data'])
 
-#     expense = Expense.objects.get(expense_id='txhJLOSKs1iN', org_id=org_id)
-#     assert expense.category == 'ABN Withholding'
+    expense = Expense.objects.get(expense_id='txhJLOSKs1iN', org_id=org_id)
+    assert expense.category == 'ABN Withholding'
 
-#     expense.exported = True
-#     expense.category = 'Old Category'
-#     expense.save()
+    expense.exported = True
+    expense.category = 'Old Category'
+    expense.save()
 
-#     update_non_exported_expenses(payload['data'])
-#     expense = Expense.objects.get(expense_id='txhJLOSKs1iN', org_id=org_id)
-#     assert expense.category == 'Old Category'
+    update_non_exported_expenses(payload['data'])
+    expense = Expense.objects.get(expense_id='txhJLOSKs1iN', org_id=org_id)
+    assert expense.category == 'Old Category'
 
-#     try:
-#         update_non_exported_expenses(payload['data'])
-#     except ValidationError as e:
-#         assert e.detail[0] == 'Workspace mismatch'
+    try:
+        update_non_exported_expenses(payload['data'])
+    except ValidationError as e:
+        assert e.detail[0] == 'Workspace mismatch'
 
+#     uncomment this while using the webhook callback for edit expense
 #     url = reverse('webhook-callback', kwargs={'workspace_id': 1})
 #     response = api_client.post(url, data=payload, format='json')
 #     assert response.status_code == status.HTTP_200_OK
